38b097416b1aa9ce7e0c6de1d388150d7b941420
pcleon <wpctszz@qq.com>
2014-08-11 23:29:51 +0800

    Update exam.py
    终结版1,下一步争取自动回答.......-_-#

--- shell/python/exam.py
+++ shell/python/exam.py
@@ -1,48 +1,68 @@
 #!/usr/bin/python
 #coding:utf-8
-BASE_URL = '' #考试的基地址
+
 import urllib2
 import re
 import sys
 from bs4 import BeautifulSoup
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from optparse import OptionParser
 
+#查找要答题的网页的所有题目
+def findQestion(req_url):
+    soup = BeautifulSoup(req_url)
+    req = []
+    s1 = soup.find_all('tr',height='25')
+    for line in s1:
+        reg = u"\d分"
+        if re.search(reg,line.get_text()):
+            req += [line.td.get_text().split('\n')[0]]
+    return req
+
+#利用问题去匹配已有答案的页面,找出答案-_-
 def getAnser(question , webcontent ):
     soup = BeautifulSoup(webcontent)
     s1 = soup.find_all('tr',height='25')
     ans = []
 
     for line in s1:
-        if re.search(question.encode('utf-8'), str(line)):
-            matchline = line
-            for choice in matchline.td.find_all('img', src = re.compile(u's.gif$')):
-               if re.search('ed_s.gif',str(choice)):
+        if re.search(question, line.get_text()):
+            for choice in line.td.find_all('img', src = re.compile(u's.gif$')):
+               if re.search(u'ed_s.gif',str(choice)):
                    ans += [1]
                else:
                    ans += [0]
             return ans
             break
 
-def findQestion(req_url):
-    soup = BeautifulSoup(req_url)
-    req = []
-    s1 = soup.find_all('tr',height='25')
-    for line in s1:
-        if re.search('\d分',str(line)):
-            req += [line.td.get_text().split()[0]]
-    return req
 
 
 def main():
-    uri = sys.argv[1]
-    html=urllib2.urlopen(BASE_URL + uri)
-    s=html.read()
-    content = s.decode('gb2312').encode('utf-8')
-    qes = findQestion(content)
-    for qnum in qes:
-        print qnum
-        print getAnser(qnum,content)
+    usage = "usage: %prog -a args -q args"
+    parser = OptionParser(usage)
+    parser.add_option('-q', '--question',
+                        action='store', dest='question', type='string',
+                        help='the question web page ')
+    parser.add_option('-a', '--answer',
+                        action='store', dest='answer', type='string',
+                        help='the answered web page ')
+    options, args = parser.parse_args()
+
+    try:
+        ans_url = options.answer
+        ask_url = options.question
+
+        ans_html=urllib2.urlopen(ans_url).read()
+        ans_content = ans_html.decode('gb2312')
+
+        ask_html=urllib2.urlopen(ask_url).read()
+        ask_content = ask_html.decode('gb2312')
+
+        asks = findQestion(ask_content)
+        for ask in asks:
+            print ask
+            print getAnser(ask[5:],ans_content)
+    except:
+        parser.print_help()
 
 
 
