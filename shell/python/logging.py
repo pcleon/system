6ff01b42bc4f3b2415d71df73417aaa171c60a9b
pcleon <wpctszz@qq.com>
2017-10-17 11:21:38 +0800

    Update logging.py

--- shell/python/logging.py
+++ shell/python/logging.py
@@ -2,31 +2,33 @@
 #coding:utf8
 
 import logging
-import time
 from logging.handlers import TimedRotatingFileHandler
+import time
 
-def initlog():
-    #日志打印格式
-    log_fmt = '%(asctime)s\tFile \"%(filename)s\",line %(lineno)s\t%(levelname)s: %(message)s'
-    logging.basicConfig(level=logging.INFO)
-    formatter = logging.Formatter(log_fmt)
-    log = logging.getLogger()
-    #将默认输出删除,只记录到日志
-    log.handlers = []
-    #创建TimedRotatingFileHandler对象
-    log_file_handler = TimedRotatingFileHandler(filename="test", when="midnight", interval=1, backupCount=10)
-    log_file_handler.suffix = "%Y-%m-%d_%H_%M.log"
-    log_file_handler.setFormatter(formatter)
-    log.addHandler(log_file_handler)
-    return log
+def initLog():
+    #logger
+    logger = logging.getLogger('test')
+    logger.setLevel(logging.WARNING)
+    #file handler
+    fname = 'log'
+    fh = TimedRotatingFileHandler(filename = fname , when='M', interval= 1 , backupCount= 0)
+    fh.suffix = "%Y%m%d_%H%M"
+    #fmt
+    datefmt = "%Y%m%d %H:%M:%S"
+    fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(message)s"
+    formatter = logging.Formatter(fmt, datefmt)
+    #add fmt to handler
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
 
-
-def test():
-   logger.debug('debug')
-   logger.info('info')
-   logger.warn('warning')
-
-logger = initlog()
-while 1:
-    test()
-    time.sleep(2)
+while 1 :
+    t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    info = 'info ---- ' + t
+    warn = 'info ---- ' + t
+    err = 'info ---- ' + t
+    log = initLog()
+    log.info(info)
+    log.warning(warn)
+    log.error(err)
+    time.sleep(1)
